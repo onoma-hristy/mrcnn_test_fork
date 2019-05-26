1878285fc22c0788ac3ac557ec1c40fe858e9adf
rianpramudia <rian.pramudia@gmail.com>
2019-05-27 00:23:04 +0700

    small pyramid 1024x1024

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -52,7 +52,7 @@ class Config(object):
     # You can also provide a callable that should have the signature
     # of model.resnet_graph. If you do so, you need to supply a callable
     # to COMPUTE_BACKBONE_SHAPE as well
-    BACKBONE = "resnet101"
+    BACKBONE = "resnet50"
 
     # Only useful if you supply a callable to BACKBONE. Should compute
     # the shape of each layer of the FPN Pyramid.
@@ -67,7 +67,7 @@ class Config(object):
     FPN_CLASSIF_FC_LAYERS_SIZE = 1024
 
     # Size of the top-down layers used to build the feature pyramid
-    TOP_DOWN_PYRAMID_SIZE = 256
+    TOP_DOWN_PYRAMID_SIZE = 128
 
     # Number of classification classes (including background)
     NUM_CLASSES = 1  # Override in sub-classes
@@ -125,7 +125,7 @@ class Config(object):
     #         size IMAGE_MIN_DIM x IMAGE_MIN_DIM. Can be used in training only.
     #         IMAGE_MAX_DIM is not used in this mode.
     IMAGE_RESIZE_MODE = "square"
-    IMAGE_MIN_DIM = 768
+    IMAGE_MIN_DIM = 1024
     IMAGE_MAX_DIM = 1024
     # Minimum scaling ratio. Checked after MIN_IMAGE_DIM and can force further
     # up scaling. For example, if set to 2 then images are scaled up to double
